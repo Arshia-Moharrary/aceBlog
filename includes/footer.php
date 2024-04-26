9978bd2ac344cbbe6cc2db8576f68c726fcaf5ef
Arshia-Moharrary <arshia.moharrary@gmail.com>
2024-04-26 14:55:46 +0430

    add footer to the project

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<br>
+<footer class="bg-body-tertiary text-center text-lg-start">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.08);">
+        © <?= date("Y") ?> Copyright:
+        <a class="text-body" href="https://github.com/Arshia-Moharrary">Arshia Moharrary</a>
+    </div>
+</footer>
