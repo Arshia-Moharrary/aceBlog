93edd23efea37ef95b6a698c2efaf94e3fb6f0a8
Arshia-Moharrary <arshia.moharrary@gmail.com>
2024-04-26 15:38:25 +0430

    add sign up page to the project

---
+++ users/signup/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <title>Sign up</title>
+    <style>
+        body {
+            padding: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <p class="fw-bold fs-2">Sign up form</p>
+    <form class="row g-3" action="" method="post">
+        <div class="col-md-6">
+            <label for="firstNameInput" class="form-label">First name</label>
+            <input type="text" class="form-control" id="firstNameInput" name="firstName">
+        </div>
+        <div class="col-md-6">
+            <label for="lastNameInput" class="form-label">Last name</label>
+            <input type="text" class="form-control" id="lastNameInput" name="lastName">
+        </div>
+        <div class="col-12">
+            <label for="emailInput" class="form-label">Email</label>
+            <input type="email" class="form-control" id="emailInput" name="email">
+        </div>
+        <div class="col-12">
+            <label for="passwordInput" class="form-label">Password</label>
+            <input type="password" class="form-control" id="passwordInput" name="password">
+        </div>
+        <div class="col-12">
+            <label for="passwordInput" class="form-label">Repeat password</label>
+            <input type="password" class="form-control" id="passwordInput" name="repeatPassword">
+        </div>
+        <div class="col-md-6">
+            <button type="submit" class="btn btn-primary" name="sign">Sign up</button>
+        </div>
+    </form>
+    <script src="../../js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
