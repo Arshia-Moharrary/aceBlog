97ed7b19f3d77d023c8d5170a59914119d25efea
Arshia-Moharrary <arshia.moharrary@gmail.com>
2024-04-26 14:40:24 +0430

    add blogs section to website

--- index.php
+++ index.php
@@ -6,10 +6,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>Document</title>
+    <style>
+        body {
+            padding: 10px;
+        }
+    </style>
 </head>
 
 <body>
     <?php include "includes/header.php"; ?>
+    <br>
+    <p class="fs-2 text-center">Blogs</p>
+    <div class="container text-center">
+        <div class="row g-2">
+            <div class="col-3">
+                <div class="card col">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw4UeEjjERyEVTOIaXIKHlj7snPZAKulH5-z1Kau1lsw&s" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">First blog</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, in.</p>
+                        <a href="#" class="btn btn-primary">Read</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card col">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw4UeEjjERyEVTOIaXIKHlj7snPZAKulH5-z1Kau1lsw&s" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">First blog</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, in.</p>
+                        <a href="#" class="btn btn-primary">Read</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card col">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw4UeEjjERyEVTOIaXIKHlj7snPZAKulH5-z1Kau1lsw&s" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">First blog</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, in.</p>
+                        <a href="#" class="btn btn-primary">Read</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card col">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw4UeEjjERyEVTOIaXIKHlj7snPZAKulH5-z1Kau1lsw&s" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">First blog</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, in.</p>
+                        <a href="#" class="btn btn-primary">Read</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 
