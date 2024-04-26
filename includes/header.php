a8b59eb9f0d30a477837eb1d7e9ec79f9a26bc2c
Arshia-Moharrary <arshia.moharrary@gmail.com>
2024-04-26 16:06:38 +0430

    change header front-end

--- includes/header.php
+++ includes/header.php
@@ -3,32 +3,33 @@
 // For add 'ative' class to active link
 
 if ($_SERVER["PHP_SELF"] == "/index.php") {
-    $home = "active";
+    $home = "dark";
+} else {
+    $home = "secondary";
 }
 
 if ($_SERVER["PHP_SELF"] == "/about.php") {
-    $about = "active";
+    $about = "dark";
+} else {
+    $about = "secondary";
 }
 
 ?>
 
-<nav class="navbar navbar-expand-lg border-bottom border-black" style="--bs-border-opacity: .3;">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/index.html">ACE</a>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link <?= $home ?>" aria-current="page" href="/index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?= $about ?>" href="/about.php">About me</a>
-                </li>
-            </ul>
-            <div class="d-flex">
-                <a href="users/signup/signup.php" class="btn btn-success me-2">Sign up</a>
-                <a href="users/login/login.php" class="btn btn-primary">Login</a>
-            </div>
+<div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none fw-bold">
+            ACE
+        </a>
+
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="/" class="nav-link px-2 link-<?= $home ?>">Home</a></li>
+            <li><a href="/about.php" class="nav-link px-2 link-<?= $about ?>">About</a></li>
+        </ul>
+
+        <div class="col-md-3 text-end">
+            <a class="btn btn-outline-primary me-2" href="users/login/">Login</a>
+            <a class="btn btn-primary" href="users/signup/">Sign-up</a>
         </div>
-    </div>
-</nav>
-<br>
+    </header>
+</div>
