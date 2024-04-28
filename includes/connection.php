c120d0f6ed69144db5e25a60b679850ab8b98d6a
Arshia-Moharrary <arshia.moharrary@gmail.com>
2024-04-28 13:42:44 +0430

    edit variable name for conflict

--- includes/connection.php
+++ includes/connection.php
@@ -6,23 +6,26 @@
 </style>
 <?php
 
+// Includes
+require_once "ui.php";
+
 // Database info
 $rdbms = "mysql"; // Change this if your database platform isn't mysql
 $host = "localhost"; // Change this if your database isn't on your local computer (put your host ip)
 $database = "aceBlog"; // Change this if you edit database name
-$username = "root"; // Put your database username
-$password = ""; // Put your database password
+$serverUsername = "root"; // Put your database username
+$serverPassword = ""; // Put your database password
 
 // Start connection operation
 try {
     // Make connection
-    $conn = new PDO("{$rdbms}:host={$host};dbname={$database}", $username, $password);
+    $conn = new PDO("{$rdbms}:host={$host};dbname={$database}", $serverUsername, $serverPassword);
 
     // Set error mode for catch block
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Optional
-    echo "<div class='alert alert-success' role='alert'>Connected successfully</div>";
+    // echo success("Connected successfully");
 } catch (PDOException $e) {
-    echo "<div class='alert alert-danger' role='alert'>Connection failed: {$e->getMessage()} (Please check database config section in README.md file)</div>";
+    echo error("Connection failed: {$e->getMessage()} (Please check database config section in README.md file)");
 }
