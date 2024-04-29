ad9b262f72f3fe5a7d8a1ce79887e5b3a6906e19
Arshia-Moharrary <arshia.moharrary@gmail.com>
2024-04-29 20:13:11 +0430

    add blog page (front) to the website

---
+++ blogs/blog.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title>Blog</title>
+    <style>
+        body {
+            padding: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <p class="fs-1 fw-bold">Blog title</p>
+    <br>
+    <p class="fs-3 fw-semibold">Author:</p>
+    <a class="list-group-item list-group-item-action d-flex align-items-center text-bg-secondary rounded p-2" href="https://github.com/mdo">
+        <img src="https://github.com/mdo.png" alt="@mdo" width="32" height="32" class="rounded me-2" loading="lazy">
+        <span>
+            <strong>Arshia Moharrary</strong>
+        </span>
+    </a>
+    <br>
+</body>
+
+</html>
