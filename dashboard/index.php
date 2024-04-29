3f728a3c2ca6f5fd162194875b4137710d1928e0
Arshia-Moharrary <arshia.moharrary@gmail.com>
2024-04-29 13:03:11 +0430

    add front-end to dashboard page

--- dashboard/index.php
+++ dashboard/index.php
@@ -16,6 +16,41 @@
 
 <body>
     <?php include "../includes/header.php" ?>
+    <div class="d-flex justify-content-center row g-1">
+        <div class="col-2">
+            <div class="card col">
+                <a href="?section=account_detail" class="btn btn-info">Account detail</a>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="card col">
+                <a href="?section=logout" class="btn btn-danger">Logout</a>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="d-flex justify-content-center">
+        <table class="table text-center">
+            <tbody>
+                <tr>
+                    <th scope="row">Username</th>
+                    <td>arshia_moharrary</td>
+                </tr>
+                <tr>
+                    <th scope="row">Email</th>
+                    <td>arshiabd1388@gmail.com</td>
+                </tr>
+                <tr>
+                    <th scope="row">Status</th>
+                    <td class="text-success">enable</td>
+                </tr>
+                <tr>
+                    <th scope="row">Role</th>
+                    <td class="text-primary">user</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
     <?php include "../includes/footer.php" ?>
     <script src="../js/bootstrap.bundle.min.js"></script>
 </body>
