6d2c738a9b29ea6e03e9b6b0017296d504cde9a7
Arshia-Moharrary <arshia.moharrary@gmail.com>
2024-05-05 00:36:43 +0430

    edit comments

--- includes/dbinfo.php
+++ includes/dbinfo.php
@@ -1,8 +1,8 @@
 <?php
 
 // Database info
-$rdbms = "mysql"; // Change this if your database platform isn't mysql
-$host = "localhost"; // Change this if your database isn't on your local computer (put your host ip)
-$database = "aceBlog"; // Change this if you edit database name
+$rdbms = "mysql"; // Your rdbms like: mysql, microsoft sql, sql server and ...
+$host = "localhost"; // Your host ip (localhost for your local computer)
+$database = "aceBlog"; // Your database name
 $serverUsername = "root"; // Put your database username
 $serverPassword = ""; // Put your database password
