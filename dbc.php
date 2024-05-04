a1a774059a26b994a8efc54fa660ad496c0aa16b
Arshia-Moharrary <arshia.moharrary@gmail.com>
2024-05-05 00:34:47 +0430

    fix a bug

--- dbc.php
+++ dbc.php
@@ -30,8 +30,22 @@
 include "includes/ui.php";
 
 if (isset($_POST["conf"])) {
-    // Make connection
-    require_once "includes/connection.php";
+    // Includes
+    include "dbinfo.php";
+
+    // Start connection operation
+    try {
+        // Make connection
+        $conn = new PDO("{$rdbms}:host={$host}", $serverUsername, $serverPassword);
+
+        // Set error mode for catch block
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Optional
+        // echo success("Connected successfully");
+    } catch (PDOException $e) {
+        echo error("Connection failed: {$e->getMessage()} (Please check database config section in README.md file)");
+    }
 
     // Error container
     $message = [];
